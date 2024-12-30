aa6f6eb4dbbff9e224a3d333b19bd56e3f969edf
ankan_21 <adas34965@gmail.com>
2024-12-30 17:22:12 +0530

    refactor: update ESLint configuration to use __dirname for baseDirectory and extend Next.js rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      // Disable the @typescript-eslint/no-explicit-any rule
+      "@typescript-eslint/no-explicit-any": "off",  // or "warn" if you want to show a warning instead
     },
-  }),
-]
+  },
+];
 
-export default eslintConfig
+export default eslintConfig;
